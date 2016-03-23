cbfe0944c4a52be54482c57b5bf91ab6e061bc3d
Martijn van Gorp <mvgorp@gmail.com>
2016-03-23 11:56:43 +0100

    Plot1

---
+++ plot1.R
@@ -0,0 +1,17 @@
+# Load fulldata, if not yet available
+#if(!exists('fulldata')) { 
+    fulldata = read.table('household_power_consumption.txt', header = TRUE, sep = ";", na.strings = "?")
+#}
+
+# Reformat Date
+fulldata$Date <- as.Date( as.character(fulldata$Date), "%d/%m/%Y")
+
+# Subset 2 days
+data = subset(fulldata, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+    
+# Build Graph
+with(data, hist(Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)"))
+
+# Make PNG
+dev.copy(png, file = "plot1.png", width = 480, height = 480)
+dev.off()
