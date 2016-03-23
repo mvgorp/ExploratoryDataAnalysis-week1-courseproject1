aee609d66e5a0633b110fe3a0ec195d3a738a052
Martijn van Gorp <mvgorp@gmail.com>
2016-03-23 12:43:29 +0100

    Plot 2 + minor fixes

--- plot1.R
+++ plot1.R
@@ -1,13 +1,13 @@
 # Load fulldata, if not yet available
-#if(!exists('fulldata')) { 
+if(!exists('fulldata')) { 
     fulldata = read.table('household_power_consumption.txt', header = TRUE, sep = ";", na.strings = "?")
-#}
+}
 
 # Reformat Date
-fulldata$Date <- as.Date( as.character(fulldata$Date), "%d/%m/%Y")
+fulldata$Date2 = as.Date( as.character(fulldata$Date), "%d/%m/%Y")
 
 # Subset 2 days
-data = subset(fulldata, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+data = subset(fulldata, Date2 >= as.Date("2007-02-01") & Date2 <= as.Date("2007-02-02"))
     
 # Build Graph
 with(data, hist(Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)"))
---
+++ plot2.R
@@ -0,0 +1,21 @@
+# Load fulldata, if not yet available
+if(!exists('fulldata')) { 
+    fulldata = read.table('household_power_consumption.txt', header = TRUE, sep = ";", na.strings = "?")
+    
+    # Reformat Date
+    fulldata$Date2      = as.Date( as.character(fulldata$Date), "%d/%m/%Y")
+    fulldata$Datetime   = strptime( paste(as.character(fulldata$Date),as.character(fulldata$Time)), "%d/%m/%Y %H:%M:%S")
+}
+
+# Subset 2 days
+data = subset(fulldata, Date2 >= as.Date("2007-02-01") & Date2 <= as.Date("2007-02-02"))
+
+# Build graph
+with(data, {
+    plot(Datetime, Global_active_power, pch = ".", ylab = "Global Active Power (kilowatts)", xlab = "")
+    lines(Datetime, Global_active_power)
+})
+
+# Make PNG
+dev.copy(png, file = "plot2.png", width = 480, height = 480)
+dev.off()
